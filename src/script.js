89e2c52b9714bbad98be10fca0fe4e21a6c8192b
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-06-25 03:31:52 +0400

    1/3

--- src/script.js
+++ src/script.js
@@ -7,6 +7,23 @@ const total = document.querySelector(".total");
 const quantity = document.querySelector(".quantity");
 const main = document.querySelector(".main");
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 openShopping.addEventListener("click", () => {
   body.classList.add("active");
 });
