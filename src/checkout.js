02d268f87b6f3f8fbb77cf0782e5fe37efc5fd6f
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-06-28 23:45:46 +0400

    working on checkout page

--- src/checkout.js
+++ src/checkout.js
@@ -16,11 +16,24 @@ function createListCard() {
     const product = listCards[key];
     if (product != null) {
       let newDiv = document.createElement("div");
+      newDiv.classList.add("mt-5");
       newDiv.innerHTML = `
-        <div class="product_img"><img src="${product.image}" /></div>
-        <div class="text-white p-2">${product.title}</div>
-        <div class="text-white">${product.price.toLocaleString()}$</div>
-        <div class="text-white">${product.quantity}</div>
+        <div class="flex">
+          <div>
+            <div class="product_img"><img class="" src="${
+              product.image
+            }" /></div>
+          </div>
+          <div class="info-prod">
+            <div class="prod-title">${product.title}</div>
+            <div class="quantity mt-3 d-flex">
+               <button onclick="decrementQuantity(${key})">-</button>
+               <div class="count ">${product.quantity}</div>
+               <button onclick="incrementQuantity(${key})">+</button>
+            </div>
+             <div class="mt-3 font-price">${product.price.toLocaleString()}$</div>
+          </div>
+        </div>
       `;
       listCard.appendChild(newDiv);
     }
@@ -29,10 +42,38 @@ function createListCard() {
 
 function calculateTotalPrice() {
   totalPrice = Object.values(listCards).reduce(
-    (total, product) => total + product.price,
+    (total, product) => total + product.price * product.quantity,
     0
   );
   total.innerText = totalPrice.toLocaleString() + "$";
+}
+
+function saveDataToLocalStorage() {
+  localStorage.setItem("listCards", JSON.stringify(listCards));
+  calculateTotalPrice();
+}
+
+window.incrementQuantity = function (key) {
+  listCards[key].quantity++;
+  saveDataToLocalStorage();
+  reloadCard();
+};
+
+window.decrementQuantity = function (key) {
+  if (listCards[key].quantity > 1) {
+    listCards[key].quantity--;
+    saveDataToLocalStorage();
+    reloadCard();
+  } else {
+    delete listCards[key];
+    saveDataToLocalStorage();
+    reloadCard();
+  }
+};
+
+function reloadCard() {
+  listCard.innerHTML = "";
+  createListCard();
 }
 
 loadDataFromLocalStorage();
