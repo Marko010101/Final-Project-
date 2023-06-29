9f7331a516b43d483cb8909d0cf9585b1f3a65ad
Marko010101 <129887087+Marko010101@users.noreply.github.com>
2023-06-30 01:58:00 +0400

    creating admin page

---
+++ src/admin.js
@@ -0,0 +1,130 @@
+var myIndex = 0;
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import {
+  getStorage,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+import { ref as sRef } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2OKGhqRYKTeNRGv6kyf3mU2ofmc0sFE4",
+  authDomain: "style-maven.firebaseapp.com",
+  databaseURL: "https://style-maven-default-rtdb.firebaseio.com",
+  projectId: "style-maven",
+  storageBucket: "style-maven.appspot.com",
+  messagingSenderId: "389697690690",
+  appId: "1:389697690690:web:64a7052084dfe9f3efa1db",
+  measurementId: "G-VZFSLN8VCW",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const productSubmit = document.getElementById("createProd");
+
+productSubmit.addEventListener("click", (e) => {
+  const company = document.getElementById("company").value;
+  const model = document.getElementById("model").value;
+  const year = document.getElementById("year").value;
+  const color = document.getElementById("color").value;
+  const price = document.getElementById("price").value;
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+  console.log(file);
+
+  let timestamp = new Date().getTime();
+  const fileName = timestamp + file?.name;
+  console.log("test", fileName);
+
+  if (file === undefined) {
+    const uid = Math.floor(Math.random() * 100000000000000000);
+
+    set(ref(database, "/products/clothes/" + uid), {
+      company: company,
+      model: model,
+      year: year,
+      color: color,
+      img: null,
+      id: uid,
+      price: price,
+    })
+      .then(() => {
+        alert("Product Added");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert(error);
+      });
+  } else {
+    const uid = Math.floor(Math.random() * 100000000000000000);
+    const storageRef = sRef(storage, "images/" + fileName);
+
+    uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log(snapshot);
+        console.log("Uploaded a blob or file!");
+      })
+      .then(() => {
+        getDownloadURL(storageRef)
+          .then((url) => {
+            console.log(url);
+            document.getElementById("image").innerHTML = `
+            <img class="product_image mt-3" src="${url}" alt="" />
+          `;
+            set(ref(database, "/products/clothes/" + uid), {
+              company: company,
+              model: model,
+              year: year,
+              color: color,
+              img: url ? url : "",
+              id: uid,
+              price: price,
+            });
+          })
+          .then(() => {
+            alert("Product Added");
+          })
+          .catch((error) => {
+            console.error(error);
+            alert(error);
+          });
+      });
+  }
+});
+if (localStorage.getItem("accessToken")) {
+} else {
+  window.location.href = "register.html";
+  alert(
+    "To access the full functionality of our website, please register and create an account. We're excited to have you on board!"
+  );
+}
---
+++ src/register.js
@@ -0,0 +1,103 @@
+var myIndex = 0;
+
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import {
+  getDatabase,
+  set,
+  ref,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import {
+  getStorage,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+import { ref as sRef } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-storage.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2OKGhqRYKTeNRGv6kyf3mU2ofmc0sFE4",
+  authDomain: "style-maven.firebaseapp.com",
+  databaseURL: "https://style-maven-default-rtdb.firebaseio.com",
+  projectId: "style-maven",
+  storageBucket: "style-maven.appspot.com",
+  messagingSenderId: "389697690690",
+  appId: "1:389697690690:web:64a7052084dfe9f3efa1db",
+  measurementId: "G-VZFSLN8VCW",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+
+// registation
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(email.value, password.value);
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      // Registration
+      const user = userCredential.user;
+      localStorage.setItem("accessToken", user.accessToken);
+      console.log(user);
+      alert("You have registered successfully!");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+});
+
+const signEmail = document.getElementById("signemail");
+const signPassword = document.getElementById("signpassword");
+const signIn = document.getElementById("signIn");
+
+signIn.addEventListener("submit", (e) => {
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, signEmail.value, signPassword.value)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user.accessToken);
+      localStorage.setItem("accessToken", user.accessToken);
+
+      if (user) {
+        window.location.href = "admin.html";
+      } else {
+        alert("Email or Password is incorrect");
+      }
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("An error occurred during sign-in. Please try again.");
+    });
+});
+
+if (localStorage.getItem("accessToken")) {
+  window.location.href = "admin.html";
+}
